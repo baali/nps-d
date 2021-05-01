cb279fe5b0eef90c7e2ba9e2321ee78af24e8788
baali <baali@users.noreply.github.com>
2021-05-01 12:33:30 +0000

    deploy: ad2c151e520fff3562111f4a811e547ac8eb5ed3

--- icici-plots.py
+++ icici-plots.py
@@ -3,9 +3,7 @@ import pandas as pd
 from datetime import date
 
 from bokeh.plotting import figure, output_file, save
-from bokeh.models import ColumnDataSource, HoverTool
-
-icici_p = figure(title = "ICICI Prudential Pension Fund", plot_height=500, plot_width=500, tools=["pan,reset,wheel_zoom"])
+from bokeh.models import ColumnDataSource
 
 today = '{}-{}-{}'.format(
     date.today().strftime('%d'),
@@ -14,23 +12,31 @@ today = '{}-{}-{}'.format(
 
 icici_inception_date = '18-MAY-2009'
 
-# curl "https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest?fundCode=C1&startDate=21-NOV-04&endDate=24-APR-21" | jq > data/icici/icici-tier-1-scheme-c.json
+# curl 'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest?fundCode=C1&startDate=21-NOV-04&endDate=24-APR-21' | jq > data/icici/icici-tier-1-scheme-c.json
 icici_data = requests.get(
-    "https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest",
-    params={"fundCode": "C1",
-            "startDate": icici_inception_date,
-            "endDate": today}).json()
+    'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest',
+    params={'fundCode': 'C1',
+            'startDate': icici_inception_date,
+            'endDate': today}).json()
 
-df = pd.DataFrame(icici_data)
-df = df.rename(columns={'x': 'date', 'y': 'price'})
+def get_dates(entry):
+    return pd.to_datetime(entry['x'])
 
+def get_prices(entry):
+    return entry['y']
+
+dates_column = map(get_dates, icici_data)
+prices_column = map(get_dates, icici_data)
+
+source = ColumnDataSource({'dates': list(dates_column),
+                           'prices': list(prices_column)})
 # Plot the figure
-icici_p = figure(x_axis_type="datetime", title="ICICI Prudential Pension Fund")
+icici_p = figure(x_axis_type='datetime', title='ICICI Prudential Pension Fund', plot_height=200, plot_width=900, tools=['pan,reset,wheel_zoom'])
 icici_p.grid.grid_line_alpha=0.3
 icici_p.xaxis.axis_label = 'Date'
 icici_p.yaxis.axis_label = 'Price'
-icici_p.line("date", "price", color='#A6CEE3', legend_label='Scheme C Tier 1', source=ColumnDataSource(df))
+icici_p.line(x='dates', y='prices', source=source)
 
 # Save to HTML
-output_file("docs/icici.html", title="ICICI Prudential Pension Fund")
+output_file('docs/icici.html', title='ICICI Prudential Pension Fund')
 save(icici_p)
