7bc25d506ca1bff03a552c72fe986f3419f74900
baali <baali@users.noreply.github.com>
2021-05-02 10:39:28 +0000

    deploy: f3ee36116ee1ec73792e35eed189b2864a9b267a

--- icici-plots.py
+++ icici-plots.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import pandas as pd
 from datetime import date
@@ -5,19 +6,19 @@ from datetime import date
 from bokeh.plotting import figure, output_file, save
 from bokeh.models import ColumnDataSource
 
-today = '{}-{}-{}'.format(
-    date.today().strftime('%d'),
-    date.today().strftime('%b').upper(),
-    date.today().strftime('%y'))
-
-icici_inception_date = '18-MAY-2009'
-
-# curl 'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest?fundCode=C1&startDate=21-NOV-04&endDate=24-APR-21' | jq > data/icici/icici-tier-1-scheme-c.json
-icici_data = requests.get(
-    'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest',
-    params={'fundCode': 'C1',
-            'startDate': icici_inception_date,
-            'endDate': today}).json()
+def fetch_data(fund_code):
+    # curl 'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest?fundCode=C1&startDate=21-NOV-04&endDate=24-APR-21'
+    today = '{}-{}-{}'.format(
+        date.today().strftime('%d'),
+        date.today().strftime('%b').upper(),
+        date.today().strftime('%y'))
+    icici_inception_date = '18-MAY-2009'
+    icici_data = requests.get(
+        'https://www.iciciprupensionfund.com/pensionFunds/iprupnsnfunds/navhistory.rest',
+        params={'fundCode': fund_code,
+                'startDate': icici_inception_date,
+                'endDate': today}).json()
+    return icici_data
 
 def get_dates(entry):
     return pd.to_datetime(entry['x'])
@@ -25,21 +26,33 @@ def get_dates(entry):
 def get_prices(entry):
     return entry['y']
 
-dates_column = map(get_dates, icici_data)
-prices_column = map(get_prices, icici_data)
 
-source = ColumnDataSource({'dates': list(dates_column),
-                           'prices': list(prices_column)})
-# Plot the figure
+SCHEMES = ['A', 'C', 'E', 'G']
+TIERS = [1, 2]
+
 icici_p = figure(
     x_axis_type='datetime',
-    title='ICICI Prudential Pension Fund: Scheme:{}/Tier:{}'.format('C', 1),
+    title='ICICI Prudential Pension Fund',
     plot_height=200, plot_width=900,
-    tools=['pan,reset,wheel_zoom'])
+    tools=['pan,xbox_select,wheel_zoom,reset'])
 icici_p.grid.grid_line_alpha=0.3
 icici_p.xaxis.axis_label = 'Date'
 icici_p.yaxis.axis_label = 'Price'
-icici_p.line(x='dates', y='prices', source=source)
+
+for tier in TIERS:
+    for scheme in SCHEMES:
+        fund_code = '{}{}'.format(scheme, tier)
+        print('Fetching data for Tier: {} Scheme: {}'.format(tier, scheme))
+        try:
+            data = fetch_data(fund_code)
+        except json.decoder.JSONDecodeError:
+            print('No data for Tier: {} Scheme: {}'.format(tier, scheme))
+        else:
+            dates_column = map(get_dates, data)
+            prices_column = map(get_prices, data)
+            source = ColumnDataSource({'dates': list(dates_column),
+                                       'prices': list(prices_column)})
+            icici_p.line(x='dates', y='prices', source=source)
 
 # Save to HTML
 output_file('docs/icici.html', title='ICICI Prudential Pension Fund')
