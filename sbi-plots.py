001c6b883306f661deb659fa1ff106ed8809da24
Shantanu <shantanu@senic.com>
2021-05-07 23:48:14 +0530

    Fetch latest NAV values and append them to CSV file

--- sbi-plots.py
+++ sbi-plots.py
@@ -1,3 +1,4 @@
+from requests_html import HTMLSession
 import pandas as pd
 
 from bokeh.layouts import column
@@ -5,13 +6,37 @@ from bokeh.palettes import all_palettes
 from bokeh.plotting import figure, output_file, save
 from bokeh.models import ColumnDataSource
 
-data = pd.read_csv('data/sbi/data.csv', sep='\t')
+SBI_DATA = 'data/sbi/data.csv'
 
 SCHEMES = ['A', 'C', 'E', 'G']
 COLORS = all_palettes['Colorblind'][4]
 TIERS = ['I', 'II']
+NAV_URL = 'https://www.sbipensionfunds.com/latest-nav-2/'
+
+def update_data():
+    data = pd.read_csv(SBI_DATA, sep='\t')
+    session = HTMLSession()
+    try:
+        r = session.get(NAV_URL)
+    except:
+        raise "Failed to open SBI NAV URL"
+    else:
+        vals = []
+        for td in r.html.find('td'):
+            if td.text in data.columns:
+                continue
+            else:
+                vals.append(td.text)
+        if not [date for date in data['Date'] if date == vals[0]]:
+            print('Updating CSV with data for: {}'.format(vals[0]))
+            with open(SBI_DATA, 'a') as csv_file:
+                csv_file.write('\n')
+                csv_file.write('\t'.join(vals))
+        else:
+            print('No new data')
 
 def plot_tier(tier):
+    data = pd.read_csv(SBI_DATA, sep='\t')
     t_plot = figure(
         x_axis_type='datetime',
         title='SBI Pension Fund, Tier: {}'.format(tier),
@@ -36,6 +61,7 @@ def plot_tier(tier):
         ]
     return t_plot
 
+update_data()
 t_plots = list(map(plot_tier, TIERS))
 
 # Save to HTML
